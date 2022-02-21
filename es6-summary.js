72dd7ba0d2ab663f68c87df08b9fac82b4720d75
NishatFariza <nishatfarzia78@gmail.com>
2022-02-21 19:11:38 +0600

    template string done

---
+++ es6-summary.js
@@ -0,0 +1,19 @@
+// var number = 46;
+// var number =34;//  reasign possible
+
+let sum =34;
+// let sum =45;//resign not possible
+sum =54; //possible
+
+const number =38;
+// const number =38; [imposible]
+// number =54; //imposible
+
+
+//template-string
+let firstName ='Nishat';
+let lastName ='Fariza';
+let massage ='How are you?'
+
+let fullMassage = `Hi!, ${firstName}, ${lastName}, ${massage}`;
+console.log(fullMassage);
